95efac255659d8937eb24d47929d9f58205ab08f
Ry Ferguson <rytf12@gmail.com>
2014-09-08 16:50:05 -0700

    refactor edit account view

--- src/scrnnm/html/editAccount.php
+++ src/scrnnm/html/editAccount.php
@@ -1,47 +1,51 @@
 <?php
 
-function editAccount(array $formData, $errors = array()) {
-    return
-        '<form method="post" id="edit_account_form">
-        <input type="hidden" name="delete_flag" value="0"/>
-        <div>
-            Use this form to edit your account info.
+function editAccount(array $form_data, $errors = array()) {
+    return c\form(
+        array(
+            'method' => 'post',
+            'id' => 'edit_account_form'),
+
+        '<input type="hidden" name="delete_flag" value="0"/>',
+        c\div('Use this form to edit your account info.
             Email and new password are optional.
             You can delete your account email by submitting a blank email.
             If you change your email, then your account email will be blank
-            until you verify the new email.
-        </div>'
+            until you verify the new email.'),
 
-        . blist($errors, array('class' => 'error'))
-        . input(array(
-            'id' => 'username',
-            'value' => $formData['username']),
-            'Username')
+        c\ulist($errors, array('class' => 'error')),
+        c\dlinput(
+            'Username',
+            array(
+                'id' => 'username',
+                'value' => $form_data['username'])),
 
-        . input(array(
-            'id' => 'email',
-            'value' => $formData['email'],
-            'type' => 'email'),
-            'Email')
+        c\dlinput(
+            'Email',
+            array(
+                'id' => 'email',
+                'value' => $form_data['email'],
+                'type' => 'email')),
 
-        . input(array(
-            'id' => 'password',
-            'type' => 'password'),
-            'New Password')
+        c\dlinput(
+            'New Password',
+            array(
+                'id' => 'password',
+                'type' => 'password')),
 
-        . input(array(
-            'id' => 'confirm_password',
-            'type' => 'password'),
-            'Confirm New Password')
+        c\dlinput(
+            'Confirm New Password',
+            array(
+                'id' => 'confirm_password',
+                'type' => 'password')),
 
-        . input(array(
-            'id' => 'current_password',
-            'type' => 'password'),
-            'Current Password')
+        c\dlinput(
+            'Current Password',
+            array(
+                'id' => 'current_password',
+                'type' => 'password')),
 
-        . sprintf('<div>%s%s</div>',
+        c\div(
             '<input type="submit" value="Submit"/>',
-            '<input type="button" value="Delete Account" onclick="deleteAccount();"/>')
-
-        . '</form>';
+            '<input type="button" value="Delete Account" onclick="deleteAccount();"/>'));
 }
