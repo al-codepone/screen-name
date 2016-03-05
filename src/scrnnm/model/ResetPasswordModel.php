dab501eeb46c0b671d2ce21c8e3f7d8fbf329516
Ry Ferguson <rytf12@gmail.com>
2016-03-05 03:25:32 -0800

    replace additionalHeaders with additional_headers(again I think)

--- src/scrnnm/model/ResetPasswordModel.php
+++ src/scrnnm/model/ResetPasswordModel.php
@@ -16,12 +16,12 @@ class ResetPasswordModel extends TokenModel {
         if($user_data) {
             $token = \pc\sha1_token();
             $subject = 'Reset Your Password';
-            $additionalHeaders = sprintf("From: %s\r\n", EMAIL_FROM);
+            $additional_headers = sprintf("From: %s\r\n", EMAIL_FROM);
             $message = sprintf("%s,\n\nUse this link to reset your password:\n\n%s%s%d/%s",
                 $user_data['username'], SITE, RESET_PASSWORD, $user_data['user_id'], $token);
 
             parent::createToken($user_data['user_id'], $token);
-            email($email, $subject, $message, $additionalHeaders);
+            email($email, $subject, $message, $additional_headers);
         }
     }
 }
--- src/scrnnm/model/VerifyEmailModel.php
+++ src/scrnnm/model/VerifyEmailModel.php
@@ -12,11 +12,11 @@ class VerifyEmailModel extends TokenModel {
     public function createToken($user_id, $username, $email) {
         $token = \pc\sha1_token();
         $subject = 'Verify Your Email';
-        $additionalHeaders = sprintf("From: %s\r\n", EMAIL_FROM);
+        $additional_headers = sprintf("From: %s\r\n", EMAIL_FROM);
         $message = sprintf("%s,\n\nClick the link to verify your email:\n\n%s%s%d/%s",
             $username, SITE, VERIFY_EMAIL, $user_id, $token);
 
         parent::createToken($user_id, $token, $email);
-        email($email, $subject, $message, $additionalHeaders);
+        email($email, $subject, $message, $additional_headers);
     }
 }
